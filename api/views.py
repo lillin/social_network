005b581139c63bef9c30cd57eaadfefde56d599b
a.rudkovska <a.rudkovska@riseapps.biz>
2020-07-05 11:28:53 +0300

    Add todo

--- api/views.py
+++ api/views.py
@@ -72,3 +72,6 @@ class LikeAnalyticsView(ListAPIView):
 class UserActivityView(ReadOnlyModelViewSet):
     queryset = User.objects.exclude(is_active=False)
     serializer_class = UserActivitySerializer
+
+
+# TODO: add logout, inspect JWT auth
