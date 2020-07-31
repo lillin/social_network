8f0cfe88aec965ffc13d9bacb8b99ef91e68d86d
a.rudkovska <a.rudkovska@riseapps.biz>
2020-07-31 17:04:40 +0300

    Add ckeditor to admin text field

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth import get_user_model
+from ckeditor.widgets import CKEditorWidget
+
+from api.models import Post
+
+
+User = get_user_model()
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
--- social_network/settings.py
+++ social_network/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_swagger',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -171,3 +172,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
